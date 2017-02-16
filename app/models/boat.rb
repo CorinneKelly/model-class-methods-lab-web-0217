c707a5b1fa4c0425810826824c67b47ef227b7cb
CorinneKelly <corinnemariekelly@gmail.com>
2017-02-16 18:05:43 -0500

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,5 +1,40 @@
+require 'pry'
+
 class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+  	order(id: :asc).limit(5)
+  end
+
+  def self.dinghy
+  	where("length < 20")
+  end
+
+  def self.ship
+  	where("length > 20 OR length = 20")
+  end
+
+  def self.last_three_alphabetically
+  	order(name: :desc).limit(3)
+  end
+
+  def self.without_a_captain
+  	where(captain: nil)
+  end
+
+  def self.sailboats
+  	joins(:classifications).where(classifications: {name: "Sailboat"})
+  	# include(:classifications).where(classifications: {name: "Sailboat"})
+
+  end
+
+  def self.with_three_classifications
+  	boat_array = []
+  	joins(:classifications).group("boats.id").having("COUNT(*) == 3")
+
+  end
+
 end
--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,35 @@
+require 'pry'
 class Captain < ActiveRecord::Base
   has_many :boats
+  has_many :boat_classifications, through: :boats
+  has_many :classifications, through: :boat_classifications
+
+  def self.catamaran_operators
+  	  joins(:classifications).where(classifications: {name: 'Catamaran'})
+  end
+
+  def self.sailors
+  	joins(:classifications).where(classifications: {name: 'Sailboat'}).distinct
+  end
+
+  def self.talented_seamen
+  	# new_table = self.
+  	sail_id = joins(:classifications).where(classifications: {name: "Sailboat"}).pluck(:id) 
+  	motor_id = joins(:classifications).where(classifications: {name: "Motorboat"}).pluck(:id)
+  	# binding.pry
+  	all_id = sail_id & motor_id
+  	where(id: all_id)
+  end
+
+  def self.non_sailors
+  	sail_boaters = joins(:classifications).where(classifications: {name: 'Sailboat'})
+  	non_sailors = joins(:classifications).where.not(classifications: {name: 'Sailboat'})
+  	sail_boaters.map do |cap|
+  		if non_sailors.include?(cap)
+  			non_sailors.delete(cap)
+  		end
+  	end
+  	non_sailors.distinct
+  end
+
 end
